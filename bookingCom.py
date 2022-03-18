3d9180b1176f8dc8e42a227cbe069b9e4d7aabf6
esme <ecole@esme.fr>
2022-03-18 16:52:02 +0100

    on peut recuperer les notes depuis booking

--- bookingCom.py
+++ bookingCom.py
@@ -9,22 +9,23 @@ def searchCity(city):
     time.sleep(2)
     driver.find_element(by="class name", value="sb-searchbox__button ").click()
 
+def setDate(date):
+    ...
 
 def getHotels():
     time.sleep(2)
     hostelList = driver.find_elements(by="class name", value="fb01724e5b")
     hostelsNames = list(map(lambda hotel: hotel.text.split("\n")[0], hostelList))
     hostelsLinks = list(map(lambda hotel: hotel.get_attribute("href"), hostelList))
-
     grades = list(map(lambda grade: grade.text,
-                      driver.find_elements(by="class name", value="_9c5f726ff bd528f9ea6")))
+                      driver.find_elements(by="xpath", value="//div[contains(@class, '_9c5f726ff bd528f9ea6')]")))
 
-    print(grades)
 
-driver = webdriver.Firefox()
+if __name__ == '__main__':
+    driver = webdriver.Firefox()
 
-driver.get(
-    "https://www.booking.com/index.fr.html?label=gen173nr-1BCAEoggI46AdIM1gEaE2IAQGYAQ24ARfIAQzYAQHoAQGIAgGoAgO4Arf4yJEGwAIB0gIkNmMwYWYwNGUtNGY3Ni00ZTk3LThjOGUtZWQ0OTEwMDZkZGMw2AIF4AIB;sid=4870985d274b91999c83d2a5d6f77393;keep_landing=1&sb_price_type=total&")
+    driver.get(
+        "https://www.booking.com/index.fr.html?label=gen173nr-1BCAEoggI46AdIM1gEaE2IAQGYAQ24ARfIAQzYAQHoAQGIAgGoAgO4Arf4yJEGwAIB0gIkNmMwYWYwNGUtNGY3Ni00ZTk3LThjOGUtZWQ0OTEwMDZkZGMw2AIF4AIB;sid=4870985d274b91999c83d2a5d6f77393;keep_landing=1&sb_price_type=total&")
 
-searchCity("Paris")
-getHotels()
+    searchCity("Paris")
+    getHotels()
