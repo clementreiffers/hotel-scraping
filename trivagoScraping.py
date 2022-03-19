6b56527250ea6c2fdb2643b7d2042818e1e67927
Im-Rises <quentin-pimbole@hotmail.fr>
2022-03-19 18:57:47 +0100

    Updating trivago

--- trivagoScraping.py
+++ trivagoScraping.py
@@ -7,8 +7,25 @@ Created on Sat Mar  19 11:37:12 2022
 @author: QuentinM
 """
 
+# Dictionnary
+monthCorrespondances = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December",
+}
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import datetime
 import time
 
 
@@ -27,13 +44,46 @@ def writeCity(city):
     driver.find_element(by="id", value="react-autowhatever-1--item-0").click()
 
 
-def selectDate(checkInDate, checkOutDate):
+def selectDate(dateChoosen):
     time.sleep(2)
-    driver.find_element(by="xpath", value="//time[@datetime='" + checkInDate + "']").find_element(by="xpath",
-                                                                                                  value="..").click()
-    time.sleep(2)
-    driver.find_element(by="xpath", value="//time[@datetime='" + checkOutDate + "']").find_element(by="xpath",
-                                                                                                   value="..").click()
+    # Go to previous page in the calendar
+    # driver.find_element(by="xpath", value="//button[@data-testid='calendar-button-prev']").click()
+    try:
+        driver.find_element(by="xpath", value="//time[@datetime='" + dateChoosen + "']") \
+            .find_element(by="xpath", value="..") \
+            .click()
+    except:
+        # currentDate = datetime.date.today()
+        # if (dateChoosen < currentDate):
+        #     print("Date is before the current day")
+        #     exit(1)
+        # else:
+        # print("Date is working")
+        dateChoosen = dateChoosen.split('-')
+        dateChoosen = datetime.date(int(dateChoosen[0]), int(dateChoosen[1]), int(dateChoosen[2]))
+
+        dateCalendar = driver.find_element(by="xpath", value="//button[contains(@class, 'cursor-auto font-bold')]") \
+            .text.split(' ')
+        # dateCalendar = datetime.date(int(dateCalendar[0]), int(dateCalendar[1]), 1)
+        month = dateCalendar[0]
+        year = dateCalendar[1]
+        while month not in dateChoosen and year not in dateChoosen:
+        # print(dateCalendar.strftime("%Y-%M"))
+        # while dateCalendar != dateChoosen.:
+        #     if month in currentDate and year in currentDate:
+        #         isGoodMonthShows = True
+
+
+# pageDate = driver.find_element(by="xpath",value="//div[@data-testid='calendar-popover']/")
+# if pageDate < currentYear:
+#     exit('Selected date is behind the current date')
+# else:
+#     while pageDate < currentYear:
+# Press next button
+
+# time.sleep(2)
+# driver.find_element(by="xpath", value="//time[@datetime='" + checkOutDate + "']") \
+#     .find_element(by="xpath", value="..").click()
 
 
 def selectGhests(adultsNumber, childrenNumber, roomsNumber):
@@ -53,28 +103,18 @@ def selectGhests(adultsNumber, childrenNumber, roomsNumber):
     driver.find_element(by="xpath", value="//button[@data-testid='search-button']").click()
 
 
+def copyHotelsDataFromResearch():
+    print("")
+
+
 if __name__ == '__main__':
     driver = webdriver.Firefox()
     driver.maximize_window()
     driver.get("https://www.trivago.com")
     selectHoteltab()
     writeCity("Paris")
-    selectDate('2022-04-13', '2022-04-14')
-    selectGhests(5, 4, 5)
-    driver.close()
-
-    # datesListPage = driver.find_elements(by="tag name", value="time")
-    # for dateCheckIn in datesListPage:
-    #     if dateCheckIn.get_attribute("dateTime") == checkInDate:
-    #         print("Date found")
-    #         dateCheckIn.click()
-    # print(dateCheckIn.get_attribute("dateTime"))
-
-    # driver.find_element("css selector", value="time[datetime='2022-04-11']").click()
-    # checkInDate=datetime.strptime(checkInDate,"%d/%m/%y")
-    # print(checkInDate)
-    # driver.find_element(by='id',value='cal-heading-month').
-    # print(driver.find_element(by="Xpath",value="//*[@id=\"cal-heading-month\"]/span").is_selected())
-    # monthDate = driver.find_element(by="id",value="cal-heading-month")
-    # if monthDate.text==checkInDate:
-    #     driver.find_element(by="")
+    selectDate('2022-03-19')
+    selectDate('2022-03-20')
+    # selectGhests(5, 4, 5)
+    # copyHotelsDataFromResearch()
+    # driver.close()
