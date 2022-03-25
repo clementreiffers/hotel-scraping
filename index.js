092a95d41fefe19fe5c43ef7e00b78970d2be409
clementreiffers <reiffersclement@gmail.com>
2022-03-25 19:53:21 +0100

    separation du String en array

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import * as R from "ramda";
 import * as fs from "fs";
 
-// to read 'input.txt' file
+const splitData = R.pipe(
+    R.split("\n"),
+    R.map(R.split(","))
+);
+
+const transformDataToJsonLike = R.pipe(
+    
+)
+
 const data = fs.readFileSync('./bookingCom.csv',
     {encoding:'utf8', flag:'r'});
 
-console.log(data);
+console.log(splitData(data));
