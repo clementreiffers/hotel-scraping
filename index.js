613396c6a15f5ad26fd803fed6b00e4e7856d51d
clementreiffers <reiffersclement@gmail.com>
2022-03-25 20:26:48 +0100

    normalement la lecture des csv ok, mais reste à changer le format des csv qui peut causer des soucis

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import * as R from "ramda";
 import * as fs from "fs";
 
-const splitData = R.pipe(
+const splitData_ = R.pipe(
     R.split("\n"),
     R.map(R.split(","))
 );
 
-const transformDataToJsonLike = R.pipe(
-    
+const transformDataToJsonLike_ = R.applySpec({
+    name: R.head,
+    stars: R.slice(1, 2),
+    grade: R.slice(2, 3),
+    price: R.slice(3, 4),
+    localisation: R.slice(4, 5),
+    link: R.last
+})
+
+const getJson = R.pipe(
+    splitData_,
+    R.map(transformDataToJsonLike_)
 )
 
 const data = fs.readFileSync('./bookingCom.csv',
-    {encoding:'utf8', flag:'r'});
+    {encoding: 'utf8', flag: 'r'});
 
-console.log(splitData(data));
+
+console.log(getJson(data))
