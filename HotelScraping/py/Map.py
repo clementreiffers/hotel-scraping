a7df857df93fc70ca95181640de4a595ad83801e
Maaelle <maelle.marcelin@esme.fr>
2022-04-05 17:28:44 +0200

    mise à jour carte

--- HotelScraping/py/Map.py
+++ HotelScraping/py/Map.py
@@ -5,7 +5,7 @@ import numpy as np
 import folium
 
 # export du csv général
-df = pd.read_csv("hotel_scrap.csv", sep = ";")
+df = pd.read_csv("C:/Users/ACER/PycharmProjects/HotelScraping/HotelScraping/csv/test_carte.csv", sep = ";")
 
 
 # création de la carte
@@ -13,30 +13,28 @@ carte = folium.Map([48.850928, 2.346260], zoom_start=20)
 
 # suppression des lignes jumelles pout le même hotel
 df_carte = df[['gps','name','address','prices']]
-info_carte = df_carte.drop_duplicates(subset=['gps'], ignore_index = True)
+df_carte = df_carte.sort_values(by=["name"], key=lambda col: col.str.upper())
+info_carte = df_carte.drop_duplicates(subset=['name'], ignore_index = True)
+coordo_carte = info_carte.dropna(how = 'any')
+
 
 # récupération des données
-x = info_carte['gps']
-y = info_carte['name']
-z = info_carte['address']
-p = info_carte['prices']
+x = coordo_carte['gps'].values
+y = coordo_carte['name'].values
+z = coordo_carte['address'].values
+p = coordo_carte['prices'].values
 
 
 # ajout des points sur la carte
-for i in range(len(info_carte)):
-    if x[i] == 'gps':
-        None
-    elif x[i] is not  np.nan:
-        html = str(y[i]) + "<br>" + str(z[i]) + "<br>"  + str(p[i])
-        iframe = folium.IFrame(html, width=200, height=100)
-        popup = folium.Popup(iframe, max_width=200)
-        loc = x[i].split(",")
-        characters = "[]"
-        latitude = ''.join( x for x in loc[0] if x not in characters)
-        longitude = ''.join(x for x in loc[1] if x not in characters)
-        folium.Marker([latitude, longitude], popup = popup).add_to(carte)
-    else:
-        None
+for i in range(len(coordo_carte)):
+    html = str(y[i]) + "<br>" + str(z[i]) + "<br>"  + str(p[i]+"€")
+    iframe = folium.IFrame(html, width=200, height=100)
+    popup = folium.Popup(iframe, max_width=200)
+    loc = x[i].split(",")
+    characters = "[]"
+    latitude = ''.join( x for x in loc[0] if x not in characters)
+    longitude = ''.join(x for x in loc[1] if x not in characters)
+    folium.Marker([latitude, longitude], popup = popup).add_to(carte)
 
 carte.save('Carte_hotel.html')
 webbrowser.open("Carte_hotel.html")
