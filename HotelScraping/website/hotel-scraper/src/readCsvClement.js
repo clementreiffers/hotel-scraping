58c169f1b256c7c78a42e417025ece94d3ffc48f
esme <ecole@esme.fr>
2022-04-05 14:03:18 +0200

    modification du read csv mais on attend une version pandas like que quentin developpera

---
+++ HotelScraping/website/hotel-scraper/src/readCsvClement.js
@@ -0,0 +1,34 @@
+import * as R from "ramda";
+import * as fs from "fs";
+
+const splitData_ = R.pipe(
+    R.split("\n"),
+    R.map(R.split(";")),
+);
+
+const transformDataToJsonLike_ = R.applySpec({
+    name: R.head,
+    grade: R.nth(1),
+    stars: R.nth(2),
+    prices: R.nth(3),
+    address: R.nth(4),
+    gps: R.nth(5),
+    start_date: R.nth(6),
+    end_date: R.nth(7),
+    adults: R.nth(8),
+    children: R.nth(9),
+    rooms: R.nth(10),
+    link: R.last
+});
+
+
+const getJson = R.pipe(
+    splitData_,
+    R.map(transformDataToJsonLike_)
+);
+
+const data = fs.readFileSync('../../../csv/csv_par_site/booking_general.csv',
+    {encoding: 'utf8', flag: 'r'});
+
+
+console.log(getJson(data));
